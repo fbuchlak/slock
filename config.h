e770970a17a3b21dfa73e84022aa4a2e2da0d0b6
fbuchlak <30214087+fbuchlak@users.noreply.github.com>
2023-05-29 15:58:04 +0200

    patch xresources

--- config.h
+++ config.h
@@ -1,8 +1,18 @@
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT] =	"#000000", /* after initialization */
+	[INPUT] =  	"#005577", /* during input */
+	[FAILED] = 	"#CC3333", /* wrong password */
 };
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+	{ "color0",       STRING,  &colorname[INIT] },
+	{ "color4",       STRING,  &colorname[INPUT] },
+	{ "color1",       STRING,  &colorname[FAILED] },
+};
+
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
